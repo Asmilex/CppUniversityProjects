818d2e9c2e20666dc827ba0735e5ef5187332dbc
Asmilex <andresmm@outlook.com>
2017-12-18 15:13:58 +0100

    Updated 72

--- In-class work/Parte 7/72.cpp
+++ In-class work/Parte 7/72.cpp
@@ -2,14 +2,31 @@
 #include <vector>
 using namespace std;
 
-void LecturaEnteros(vector<int> & escalares){
-    for (i=0; i<escalares.size(); i++){
-        cin >>escalares[i];
+void LecturaEnteros(int dim, vector<int> & escalares){
+    int leido;
+    for (int i=1; i<dim; i++){
+        cin >>leido;
+        escalares.push_back(leido);
     }    
 }
 
 void EscribirMatriz(const vector<vector<int>> & Vandermonde){
-    for (int i=0, i<Vandermonde.size(); i++){
-        
+    unsigned int dim=Vandermonde.size();
+    for (int i=0; i<dim; i++){
+        for (int j=0; j<dim; j++)
+            cout <<Vandermonde[i][j];
+         cout <<endl;
     }
+}
+
+int main (){
+    unsigned int dim;
+    cout <<"Introduce la dimensión de la matriz: ";
+    cin >>dim;
+
+    vector<int> escalares(1,1);
+    cout <<"Ahora escribe una serie de números, que serán los escalares de la matriz";
+    LecturaEnteros(dim, escalares);
+    
+    return 0;
 }
