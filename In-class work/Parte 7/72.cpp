b855aa21d638e6329c8308b26b20c9aad88a8351
Asmilex <andresmm@outlook.com>
2017-12-18 14:40:02 +0100

    Made changes to Parte 7

---
+++ In-class work/Parte 7/72.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void LecturaEnteros(vector<int> & escalares){
+    for (i=0; i<escalares.size(); i++){
+        cin >>escalares[i];
+    }    
+}
+
+void EscribirMatriz(const vector<vector<int>> & Vandermonde){
+    for (int i=0, i<Vandermonde.size(); i++){
+        
+    }
+}
