ecb0238297eb7b30f94eaaa5f6f0dc98117f0ee8
Asmilex <andresmm@outlook.com>
2018-04-20 22:10:41 +0200

    Changed folder structure

--- Triannual2/Practicas/3-Netbeans/libreria/geometria2d/build/Debug/GNU-Linux/src/circulo.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux/src/circulo.o: src/circulo.cpp include/circulo.h \
- include/punto.h
-
-include/circulo.h:
-
-include/punto.h:
--- Triannual2/Practicas/3-Netbeans/libreria/geometria2d/build/Debug/GNU-Linux/src/punto.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux/src/punto.o: src/punto.cpp include/punto.h
-
-include/punto.h:
