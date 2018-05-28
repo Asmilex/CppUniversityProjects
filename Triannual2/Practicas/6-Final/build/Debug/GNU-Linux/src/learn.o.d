0202e5de7a8395cb12dcef2ec7219cdce28a9089
Asmilex <andresmm@outlook.com>
2018-05-28 19:58:03 +0200

    Cleaned tree

--- Triannual2/Practicas/6-Final/build/Debug/GNU-Linux/src/learn.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/src/learn.o: src/learn.cpp \
- include/ContadorBigramas.h include/Idioma.h include/Bigrama.h
-
-include/ContadorBigramas.h:
-
-include/Idioma.h:
-
-include/Bigrama.h:
